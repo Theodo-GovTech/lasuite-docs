9d101c322d900ee3971576593543b73389a2b0f6
Thomas Metzger <thomas.metzger@theodo.com>
2025-06-02 14:39:31 +0200

    🚧(frontend) embed-bloc: added bloc resizing

--- src/frontend/apps/impress/src/features/docs/doc-editor/components/custom-blocks/EmbedBlock.tsx
+++ src/frontend/apps/impress/src/features/docs/doc-editor/components/custom-blocks/EmbedBlock.tsx
@@ -8,6 +8,7 @@ import {
 import {
   BlockTypeSelectItem,
   ReactCustomBlockRenderProps,
+  ResizableFileBlockWrapper,
   createReactBlockSpec,
 } from '@blocknote/react';
 import { TFunction } from 'i18next';
@@ -151,9 +152,9 @@ export const getEmbedReactSlashMenuItems = (
         type: 'embed',
       });
     },
-    aliases: ['embed', 'iframe', 'link'],
+    aliases: ['newEmbed', 'iframeBN', 'linkBN'],
     group,
-    icon: <Icon iconName="link" $size="18px" />,
+    icon: <Icon iconName="language" $size="18px" />,
     subtext: t('Add an embed block'),
   },
 ];
@@ -164,5 +165,5 @@ export const getEmbedFormattingToolbarItems = (
   name: t('Embed'),
   type: 'embed',
   icon: () => <Icon iconName="link" $size="16px" />,
-  isSelected: (block: any) => block.type === 'embed',
+  isSelected: (block: { type: string }) => block.type === 'embed',
 });
