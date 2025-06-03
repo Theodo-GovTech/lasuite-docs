0123f7e0ecaf9d1506de4d4f00175484c92be782
Thomas Metzger <thomas.metzger@theodo.com>
2025-06-03 14:30:06 +0200

    🐛 (frontend) embed-bloc: fixed initial size is undefined

--- src/frontend/apps/impress/src/features/docs/doc-editor/components/custom-blocks/EmbedBlock.tsx
+++ src/frontend/apps/impress/src/features/docs/doc-editor/components/custom-blocks/EmbedBlock.tsx
@@ -41,7 +41,7 @@ export const iframePropSchema: PropSchema & {
   caption: { default: '' },
   name: { default: '' },
   showPreview: { default: true },
-  previewWidth: { type: 'number', default: undefined },
+  previewWidth: { default: 500 },
 };
 
 export const iframeBlockConfig = {
@@ -102,9 +102,7 @@ export const IFrameViewer = (
       ref={containerRef}
       style={{
         position: 'relative',
-        padding: '10px',
         width: '100%',
-        background: '#ff0000',
         paddingTop: `${100 / aspectRatio}%`, // padding-top sets height relative to width
       }}
     >
